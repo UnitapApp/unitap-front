c7add340764c177171f6499bfd6f94b5b90fa8fd
Ali Beigi <beigi.work@gmail.com>
2022-04-20 14:30:37 +0400

    initial components

---
+++ src/components/basic/navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <img></img>
+            <button></button>
+            <button></button>
+        </nav>
+     );
+}
+ 
+export default Navbar;
