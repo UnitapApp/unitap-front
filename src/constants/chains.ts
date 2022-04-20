99bf6af56b9c5ae40de85882f6fe7abac1f844af
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-04-20 12:16:01 +0430

    initial commit

---
+++ src/constants/chains.ts
@@ -0,0 +1,28 @@
+/**
+ * List of all the networks supported by the Uniswap Interface
+ */
+export enum SupportedChainId {
+  MAINNET = 1,
+  RINKEBY = 4,
+}
+
+export const CHAIN_IDS_TO_NAMES = {
+  [SupportedChainId.MAINNET]: 'mainnet',
+  [SupportedChainId.RINKEBY]: 'rinkeby',
+};
+
+/**
+ * Array of all the supported chain IDs
+ */
+export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = Object.values(SupportedChainId).filter(
+  (id) => typeof id === 'number',
+) as SupportedChainId[];
+
+export const SUPPORTED_GAS_ESTIMATE_CHAIN_IDS = [SupportedChainId.MAINNET];
+
+/**
+ * All the chain IDs that are running the Ethereum protocol.
+ */
+export const L1_CHAIN_IDS = [SupportedChainId.MAINNET, SupportedChainId.RINKEBY] as const;
+
+export type SupportedL1ChainId = typeof L1_CHAIN_IDS[number];
---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Web3Provider } from '@ethersproject/providers';
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
+import { NetworkContextName } from './constants/misc';
+
+function getLibrary(provider: any) {
+  return new Web3Provider(provider);
+}
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3ProviderNetwork getLibrary={getLibrary}>
+        <App />
+      </Web3ProviderNetwork>
+    </Web3ReactProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
