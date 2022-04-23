5fbda7d41b398b316c9f87ff5083ddbd820e8276
Ali Beigi <beigi.work@gmail.com>
2022-04-23 21:12:10 +0400

    add search input

---
+++ src/components/basic/Button/Input/input.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from 'styled-components/';
+import { DV } from '../../designVariables';
+
+interface props {
+  width?: string;
+}
+
+export const Input = styled.input<props>`
+  padding: ${DV.sizes.basePadding * 2}px ${DV.sizes.basePadding * 4}px;
+  width: ${(props) => props.width || 'auto'};
+  color: white;
+  background: ${DV.colors.dark};
+  border-radius: ${DV.sizes.baseRadius}px;
+  position: relative;
+  z-index: 2;
+  box-sizing: border-box;
+  &::before {
+    content: '';
+    display: block;
+    z-index: -1;
+    position: absolute;
+    background: ${DV.bgGradient.primary};
+    inset: 0;
+    margin: 0.1rem;
+    border-radius: ${DV.sizes.baseRadius * 1.5 - 1}px;
+  }
+`;
--- src/components/pages/home/components/header.tsx
+++ src/components/pages/home/components/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/'
 import { DV } from 'components/basic/designVariables'
 import { PrimaryOutlinedButton, PrimaryButton, BrightOutlinedButton } from 'components/basic/Button/button';
+import { Input } from 'components/basic/Button/Input/input';
 
 
 
@@ -22,11 +23,25 @@ const HeaderComp = styled.div`
     }
 `
 
+const FlexWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const InputWrapper = styled(FlexWrapper)`
+    position: relative;
+    bottom: ${DV.sizes.baseMargin * 2}px;
+`
+
 const Header = () => {
     return ( 
+        <>
         <HeaderComp>
             <p>Add EVM networks easily and<br/> connect your BrightID to claim Gas Fee.</p>
         </HeaderComp>
+        <InputWrapper> <Input width="360px" placeholder='Search Network / Currency'></Input> </InputWrapper>
+        </>
      );
 }
  
