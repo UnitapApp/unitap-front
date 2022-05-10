3d10e9556e71e3a6ae0eb3d5fea19a9cf58b33a6
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-11 00:29:36 +0430

    refactor: main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Home from 'components/pages/home';
 import './App.css';
-require("typeface-jetbrains-mono");
+
+import { Web3Provider } from '@ethersproject/providers';
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
+import { NetworkContextName } from './constants/misc';
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
+
+function getLibrary(provider: any) {
+  return new Web3Provider(provider);
+}
 
 function App() {
-  return <Home />;
+  return (
+    <React.StrictMode>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Web3ProviderNetwork getLibrary={getLibrary}>
+          <Home />
+        </Web3ProviderNetwork>
+      </Web3ReactProvider>
+    </React.StrictMode>
+  );
 }
 
 export default App;
