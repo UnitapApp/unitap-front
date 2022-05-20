bf4a6cc7b43a642452d8449105da9510452f60f3
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-20 14:04:23 +0430

    feat: setup sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: 'https://ebbf4d36ea304eeeb94604f18648407c@o262681.ingest.sentry.io/6422961',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
