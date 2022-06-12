7068671dcff050f6889b99ca27815d592e8fb966
Ali Beigi <beigi.work@gmail.com>
2022-06-12 10:46:10 +0400

    fix fund page responsive

--- src/pages/fund/components/Content/content.style.tsx
+++ src/pages/fund/components/Content/content.style.tsx
@@ -10,7 +10,7 @@ export const ContentCard = styled.div`
   background: ${DV.colors.black1};
   border: 1px solid ${DV.colors.border_black};
   border-radius: ${DV.sizes.baseRadius * 1.5}px;
-  width: min(30%, 430px);
+  width: min(90%, 430px);
   padding: ${DV.sizes.basePadding * 3}px ${DV.sizes.basePadding * 3}px ${DV.sizes.basePadding * 4}px;
   margin: ${DV.sizes.baseMargin * 4}px auto ${DV.sizes.baseMargin * 8}px;
   box-sizing: border-box;
