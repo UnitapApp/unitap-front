ea19869a701887db155ca20f1e2a884cda402edf
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2022-12-13 17:12:21 +0100

    NFT Header

--- src/pages/nft/components/Header/Header.tsx
+++ src/pages/nft/components/Header/Header.tsx
@@ -6,45 +6,20 @@ import { BrightIdVerificationStatus } from 'types';
 
 const Spaceship = styled.img`
   position: absolute;
-  left: 0;
-  transform: translate(-35%, -37%) scale(1);
+  right: 0;
+  transform: translate(12%, -37%);
 `;
 
 const Header = () => {
-  const { userProfile } = useContext(UserProfileContext);
 
   return (
-    <div className="header w-full h-[152px] bg-gray20 rounded-2xl flex justify-between overflow-hidden relative p-4 mb-6">
-      <Spaceship className="z-0" src="assets/images/gas-tap/header-spaceship.svg" />
-      <div className="header-left z-10 flex flex-col justify-end items-start h-[100%]">
-        <Icon className="gas-tap h-12 w-[140px] mb-1" iconSrc="assets/images/gas-tap/gas-tap-text-logo.png" />
+    <div className="header w-full h-[152px] bg-gray20 rounded-2xl flex flex-col justify-between items-start overflow-hidden relative p-6 mb-6">
+      <div className="header-top z-10 flex items-center h-auto">
+        <Icon className="gas-tap h-auto w-[305px] mb-1 mr-4" iconSrc="assets/images/nft/nft-typo-logo.png" />
+        <Icon className="gas-tap h-auto w-12 mb-1" iconSrc="assets/images/nft/nft-badge.svg" />
       </div>
-      <div className="header-right h-[100%] flex flex-col justify-end">
-        <div className="claim-stat z-10">
-          {userProfile?.verificationStatus === BrightIdVerificationStatus.VERIFIED ? (
-            userProfile.totalWeeklyClaimsRemaining == 2 ? (
-              <div className="claim-stat__not-claimed rounded-lg bg-gray30 border-2 border-gray80">
-                <p className="claim-stat__not-claimed__text px-5 py-4 text-white text-xs">
-                  You can claim <span className="claimed-left text-space-green">5</span> gas fees in this round
-                </p>
-              </div>
-            ) : (
-              <div className="claim-stat__claimed rounded-lg border-2 border-gray80 bg-primaryGradient py-[2px] px-3 flex gap-x-3">
-                <Icon iconSrc="assets/images/gas-tap/dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/empty-dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/empty-dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/empty-dabe.svg" width="36px" height="auto" />
-              </div>
-            )
-          ) : (
-            <></>
-          )}
-        </div>
-        <div className="spaceman absolute bottom-2 right-0">
-          <Icon className="z-0" iconSrc={'assets/images/claim/header-spaceman.svg'} width="120px" height="auto" />
-        </div>
-      </div>
+      <p className='gradient-text z-100 text-2xl text-gradient-primary'>Unitap Pass is a VIP pass for Unitap.</p>
+      <Spaceship className="z-0" src="assets/images/nft/nft-header-spaceship.svg" />
     </div>
   );
 };
