7051376daa5d22f3ed30cedf0fd68439ed3c092e
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2022-12-18 05:12:32 +0100

    Create BrightId Account Modal's Links Update

--- src/pages/home/components/CreateBrightIdAccountModal/createBrightIdAccountModal.tsx
+++ src/pages/home/components/CreateBrightIdAccountModal/createBrightIdAccountModal.tsx
@@ -20,11 +20,11 @@ const CreateBrightIdAccountModalContent = () => {
       <p className="text-sm text-gray100 mb-6 mr-auto">Create a BrightID account easily by following this steps:</p>
       <p className="text-sm text-white mb-4 mr-auto">1- Download BrightID App</p>
       <div className='stores flex w-full justify-evenly mb-7'>
-        <Icon className='cursor-pointer' iconSrc='assets/images/modal/google-play.svg' height='48px' width='auto' />
-        <Icon className='cursor-pointer' iconSrc='assets/images/modal/app-store.svg' height='48px' width='auto' />
+        <Icon onClick={() => window.open('https://play.google.com/store/apps/details?id=org.brightid&hl=en&gl=US&pli=1', '_blank')} className='cursor-pointer' iconSrc='assets/images/modal/google-play.svg' height='48px' width='auto' />
+        <Icon onClick={() => window.open('https://apps.apple.com/us/app/brightid/id1428946820', '_blank')} className='cursor-pointer' iconSrc='assets/images/modal/app-store.svg' height='48px' width='auto' />
       </div>
 
-      <p className="text-sm text-white mr-auto mb-32">2- Join a <span className='text-space-green underline cursor-pointer'>Verification Meet</span> to get verified by BrightID</p>
+      <p className="text-sm text-white mr-auto mb-32">2- Join a <span onClick={() => window.open('https://meet.brightid.org/#/', '_blank')} className='text-space-green underline cursor-pointer'>Verification Meet</span> to get verified by BrightID</p>
       <p className="text-xs text-gray100">then come back and connect your BrightID to enjoy Unitap</p>
     </div>
   );
