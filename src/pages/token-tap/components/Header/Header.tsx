826332d0d8be949896868346a5dab29251bbd283
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2022-12-20 00:24:01 +0100

    Token Tap Header

--- src/pages/token-tap/components/Header/Header.tsx
+++ src/pages/token-tap/components/Header/Header.tsx
@@ -15,34 +15,27 @@ const Header = () => {
 
   return (
     <div className="header w-full h-[152px] bg-gray20 rounded-2xl flex justify-between overflow-hidden relative p-4 mb-6">
-      <Spaceship className="z-0" src="assets/images/gas-tap/header-spaceship.svg" />
+      <Spaceship className="z-0" src="assets/images/token-tap/header-spaceship.svg" />
       <div className="header-left z-10 flex flex-col justify-end items-start h-[100%]">
-        <Icon className="gas-tap h-12 w-auto mb-1" iconSrc="assets/images/token-tap/token-tap-typo-logo.png" /> 
+        <span className="flex items-center mb-2">
+          <Icon className="gas-tap h-12 w-auto mb-1 mr-3" iconSrc="assets/images/token-tap/token-tap-typo-logo.png" />
+          <div className="beta border-2 rounded-lg bg-gray00 border-gray50 py-2 px-3">
+            <p className="text-gradient-primary text-xs font-semibold">Beta</p>
+          </div>
+        </span>
+        <p className="text-xs text-gray100 mb-2.5">
+          Where everyone can claim any kind of tokens such as community tokens, NFTs, UBI token.
+        </p>
       </div>
       <div className="header-right h-[100%] flex flex-col justify-end">
         <div className="claim-stat z-10">
-          {userProfile?.verificationStatus === BrightIdVerificationStatus.VERIFIED ? (
-            userProfile.totalWeeklyClaimsRemaining == 2 ? (
-              <div className="claim-stat__not-claimed rounded-lg bg-gray30 border-2 border-gray80">
-                <p className="claim-stat__not-claimed__text px-5 py-4 text-white text-xs">
-                  You can claim <span className="claimed-left text-space-green">5</span> gas fees in this round
-                </p>
-              </div>
-            ) : (
-              <div className="claim-stat__claimed rounded-lg border-2 border-gray80 bg-primaryGradient py-[2px] px-3 flex gap-x-3">
-                <Icon iconSrc="assets/images/gas-tap/dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/empty-dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/empty-dabe.svg" width="36px" height="auto" />
-                <Icon iconSrc="assets/images/gas-tap/empty-dabe.svg" width="36px" height="auto" />
-              </div>
-            )
-          ) : (
-            <></>
-          )}
+          <div className="claim-stat__not-claimed rounded-lg bg-gray30 flex p-3 pl-2">
+            <p className="text-gradient-primary text-xs mr-3"> The Final version will be Launched on March 2023. </p>
+            <p className="text-gray100 text-xs font-semibold underline cursor-pointer">Read More</p>
+          </div>
         </div>
-        <div className="spaceman absolute bottom-2 right-0">
-          <Icon className="z-0" iconSrc={'assets/images/claim/header-spaceman.svg'} width="120px" height="auto" />
+        <div className="spaceman absolute top-2 right-20">
+          <Icon className="z-0" iconSrc={'assets/images/token-tap/header-spaceman.svg'} width="100px" height="auto" />
         </div>
       </div>
     </div>
