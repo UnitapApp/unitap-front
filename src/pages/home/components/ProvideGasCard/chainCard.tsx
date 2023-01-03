e6a47182efd8f62f3c4a591a4580eadeed5f37f3
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-01-03 17:45:01 +0100

    Contract Balance Provide Gas Fee

---
+++ src/pages/home/components/ProvideGasCard/chainCard.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useMemo, useState } from 'react';
+
+import Icon from 'components/basic/Icon/Icon';
+
+import { Chain } from 'types';
+import { useWeb3React } from '@web3-react/core';
+import { BigNumber } from '@ethersproject/bignumber';
+import { useNativeCurrencyOnChain } from '../../../../hooks/useNativeCurrency';
+import JSBI from 'jsbi';
+import { CurrencyAmount } from '@uniswap/sdk-core';
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
+
+interface props {
+  chain: Chain;
+}
+
+const ChainCard = ({ chain }: props) => {
+  const { provider } = useWeb3React();
+  const [fundManagerBalance, setFundManagerBalance] = useState<BigNumber | null>(null);
+
+  useEffect(() => {
+    new StaticJsonRpcProvider(chain.rpcUrl)?.getBalance(chain.fundManagerAddress).then((balance) => {
+      setFundManagerBalance(balance);
+    });
+  }, [chain, provider]);
+
+  const nativeCurrency = useNativeCurrencyOnChain(Number(chain.chainId));
+
+  const fundManagerBalanceAmount = useMemo(() => {
+    if (!fundManagerBalance) return null;
+    const amount = JSBI.BigInt(fundManagerBalance.toString());
+    return CurrencyAmount.fromRawAmount(nativeCurrency, amount);
+  }, [nativeCurrency, fundManagerBalance]);
+
+  return (
+    <div className="chain p-5 lg:w-36 xl:w-40 sm:border-r-2 border-r-gray30">
+      <div className="chain__name flex mb-4 text-white">
+        {chain.symbol}
+        <Icon className="ml-2" iconSrc={chain.logoUrl} width="auto" height="22px" />
+      </div>
+      <p className="chain__info text-xs text-gray90 flex">
+        balance:
+        <span className="chain__info__balance text-white ml-1">
+          {fundManagerBalanceAmount ? fundManagerBalanceAmount.toSignificant(5) : '...'}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default ChainCard;
--- src/pages/home/components/ProvideGasCard/provideGasCard.tsx
+++ src/pages/home/components/ProvideGasCard/provideGasCard.tsx
@@ -1,88 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { LightOutlinedButton } from 'components/basic/Button/button';
 import RoutePath from '../../../../routes';
 import { Link } from 'react-router-dom';
 import { ClaimContext } from 'hooks/useChainList';
-import { Text } from 'components/basic/Text/text.style';
-import { ProvideGasCardWrapper } from './provideGasCard.style';
+import Icon from 'components/basic/Icon/Icon';
+import ChainCard from './chainCard';
 
 const ProvideGasCard = () => {
   const [chainListIndex, setChainListIndex] = React.useState(0);
   const { chainList } = useContext(ClaimContext);
 
-  setInterval(() => {
-    if (chainListIndex > chainList.length - 5) {
-      setChainListIndex(0);
-    } else {
-      setChainListIndex(chainListIndex + 4);
-    }
-  }, 10000);
+  useEffect(() => {
+    setTimeout(() => {
+      if (chainListIndex > chainList.length - 5) {
+        setChainListIndex(0);
+      } else {
+        setChainListIndex(chainListIndex + 4);
+      }
+    }, 10000);
+  }, [chainListIndex, chainList]);
 
-  // if (chainList.length > 0) {
-  if (true) {
+  if (chainList.length > 0) {
     return (
-      <ProvideGasCardWrapper>
-        {/* <div className="provide-gas pb-4 sm:pb-0 w-full flex flex-col sm:flex-row items-center bg-gray40 border-2 border-gray30 rounded-xl">
-      </div> */}
-        {/* <section className={'flex flex-col md:flex-row'}>
-          <div className="chain p-5 lg:w-36 xl:w-40 sm:border-r-2 border-r-gray30">
-            <div className="chain__name flex mb-4 text-white">
-              {chainList[chainListIndex].symbol}
-              <Icon className="ml-2" iconSrc={chainList[chainListIndex].logoUrl} width="auto" height="22px" />
-            </div>
-            <p className="chain__info text-xs text-gray90 flex">
-              balance: <span className="chain__info__balance text-white ml-1">1,322</span>
-            </p>
-          </div>
-          <div className="chain p-5 lg:w-36 xl:w-40 sm:border-r-2 border-r-gray30">
-            <div className="chain__name flex mb-4 text-white">
-              {chainList[chainListIndex + 1].symbol}
-              <Icon className="ml-2" iconSrc={chainList[chainListIndex + 1].logoUrl} width="auto" height="22px" />
-            </div>
-            <p className="chain__info text-xs text-gray90 flex">
-              balance: <span className="chain__info__balance text-white ml-1">1,322</span>
-            </p>
-          </div>
+      <div className="provide-gas pb-4 sm:pb-0 w-full flex flex-col sm:flex-row items-center bg-gray40 border-2 border-gray30 rounded-xl">
+        <section className={'flex flex-col md:flex-row'}>
+          <ChainCard chain={chainList[chainListIndex]} />
+          <ChainCard chain={chainList[(chainListIndex + 1) % chainList.length]} />
         </section>
         <section className={'flex flex-col md:flex-row'}>
-          <div className="chain p-5 lg:w-36 xl:w-40 sm:border-r-2 border-r-gray30">
-            <div className="chain__name flex mb-4 text-white">
-              {chainList[chainListIndex + 2].symbol}
-              <Icon
-                className="ml-2"
-                iconSrc={chainList[chainListIndex + 2].logoUrl}
-                width="auto"
-                height="22px"
-              />
-            </div>
-            <p className="chain__info text-xs text-gray90 flex">
-              balance: <span className="chain__info__balance text-white ml-1">1,322</span>
-            </p>
-          </div>
-          <div className="chain p-5 lg:w-36 xl:w-40 sm:border-r-2 border-r-gray30">
-          <div className="chain__name flex mb-4 text-white">
-          {chainList[(chainListIndex + 3) % chainList.length].symbol}
-          <Icon
-          className="ml-2"
-                iconSrc={chainList[(chainListIndex + 3) % chainList.length].logoUrl}
-                width="auto"
-                height="22px"
-              />
-              </div>
-            <p className="chain__info text-xs text-gray90 flex">
-              balance: <span className="chain__info__balance text-white ml-1">1,322</span>
-            </p>
-          </div>
-        </section> */}
-        <Text fontSize="16" mb={0} mdMb={2}>
-          <strong> Enjoying Unitap?</strong> Donate to help more users access this service
-        </Text>
+          <ChainCard chain={chainList[(chainListIndex + 2) % chainList.length]} />
+          <ChainCard chain={chainList[(chainListIndex + 3) % chainList.length]} />
+        </section>
         <Link className="m-auto" to={RoutePath.FUND}>
           <LightOutlinedButton className={'!bg-gray00 !w-48 lg:!w-60'} fontSize="14" height="46px">
             Provide Gas Fee
           </LightOutlinedButton>
         </Link>
-      </ProvideGasCardWrapper>
+      </div>
     );
   }
 
