eacd19ecb60a73bbeaf077e272fd5e6c49ac1dbe
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-01-12 00:04:05 +0100

    Add Mainnet Addresses

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -10,8 +10,10 @@ export const MULTICALL_ADDRESS: AddressMap = {
 
 export const UNITAP_PASS_BATCH_SALE_ADDRESS: AddressMap = {
   [SupportedChainId.GOERLI]: '0xC99B2Fa525E1a0C17dB4fdE3540faA1575885A8B',
+  [SupportedChainId.MAINNET]: '0x4649b7d433ee4ba472fd76073b07f082d8b18e9b',
 };
 
 export const UNITAP_PASS_ADDRESS: AddressMap = {
   [SupportedChainId.GOERLI]: '0x904018a4e9905021C1806A054E6EbD5796570131',
+  [SupportedChainId.MAINNET]: '0x4ADc8CC149A03F44386Bee80bab36F9e8022b195',
 };
