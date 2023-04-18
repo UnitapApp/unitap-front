7904ff35f5d4b35c55159cee4c92451f6dd8fa7a
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-04-18 16:28:29 +0200

    Token Tap index.tsx Clean Code

--- src/pages/token-tap/index.tsx
+++ src/pages/token-tap/index.tsx
@@ -1,35 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from 'components/common/Navbar/navbar';
-import { Row } from 'components/basic/Row/row';
-import { Col } from 'components/basic/Col/col';
-import ClaimModal from 'pages/home/components/ClaimModal/claimModal';
-import BrightConnectionModal from 'pages/home/components/BrightConnectionModal/brightConnectionModal';
-import Footer from 'components/common/Footer/footer';
-import TokensList from './components/TokensList/TokensList';
-import Header from './components/Header/Header';
-import SearchInput from './components/SearchInput/searchInput';
+import Header from "./components/Header/Header";
+import Navbar from "components/common/Navbar/navbar";
+import Footer from "components/common/Footer/footer";
+import TokensList from "./components/TokensList/TokensList";
+import SearchInput from "./components/SearchInput/searchInput";
+import ClaimModal from "pages/home/components/ClaimModal/claimModal";
+import BrightConnectionModal from "pages/home/components/BrightConnectionModal/brightConnectionModal";
 
 const TokenTap = () => {
   return (
     <>
       <Navbar />
       <div className="content-wrapper">
-        <div className="m-auto flex flex-col justify-center items-center w-full">
-          <div className="row flex wrap w-full">
-            <Header />
-          </div>
-          <Row mdReverse>
-            <Col xs={12} md={12} lg={5.5} xlg={4} className={'mt-1 lg:mt-0'}>
-              <SearchInput />
-            </Col>
-          </Row>
-          <TokensList />
-        </div>
+        <Header />
+        <SearchInput className="mt-1 lg:mt-0 w-full md:w-1/3" />
+        <TokensList />
       </div>
+      <Footer />
+
       <ClaimModal />
       <BrightConnectionModal />
-      <Footer />
     </>
   );
 };
