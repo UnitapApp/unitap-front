9e9b10731385ffef2b2bafbd41f1a0057de15a6e
Ali Maktabi <alimaktabi55@yahoo.com>
2023-08-30 12:50:20 +0330

    added proxy through vite

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,11 @@ export default ({ mode }) => {
 					target: env['REACT_APP_API_URL'],
 					changeOrigin: true,
 				},
+
+				'/enroll-prize-tap/': {
+					target: env['ENROLL_PRIZE_TAP_URL'],
+					changeOrigin: true,
+				},
 			},
 		},
 	});
