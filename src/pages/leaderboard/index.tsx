a396eadbf3307c63ed708bfba58b0b935c6e22b9
Ali Maktabi <alimaktabi55@yahoo.com>
2023-09-08 17:49:38 +0330

    removed extra navbar

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -8,39 +8,38 @@ import UsersList from './components/UsersList/usersList';
 import { users } from 'constants/usersList';
 
 const Leaderboard = () => {
-  const [records, setRecords] = useState({
-    start: users.length == 0 ? 0 : 1,
-    end: users.length >= 10 ? 10 : users.length,
-  });
+	const [records, setRecords] = useState({
+		start: users.length == 0 ? 0 : 1,
+		end: users.length >= 10 ? 10 : users.length,
+	});
 
-  let currentUser = users.filter((user) => user.userName === '@This is a test')[0];
-  let displayUser = users.slice(records.start - 1, records.end);
+	let currentUser = users.filter((user) => user.userName === '@This is a test')[0];
+	let displayUser = users.slice(records.start - 1, records.end);
 
-  const handlePagination = (start: number, end: number) => {
-    if (start > users.length || start < 1) return;
-    setRecords({ ...records, start: start, end: end >= users.length ? users.length : end });
-  };
+	const handlePagination = (start: number, end: number) => {
+		if (start > users.length || start < 1) return;
+		setRecords({ ...records, start: start, end: end >= users.length ? users.length : end });
+	};
 
-  return (
-    <>
-      <Navbar />
-      <div className="content-wrapper">
-        <div className="m-auto flex flex-col justify-center items-center w-full">
-          <div className="flex wrap w-full">
-            <Header />
-          </div>
-          <div className="bg-gray20 rounded-2xl wrap w-full p-4 ">
-            <div className="justify-between sm:flex">
-              <SearchInput />
-              <Dropdown />
-            </div>
-            <UsersList users={displayUser} currentUser={currentUser} />
-            <Pagination records={records} onClick={(start, end) => handlePagination(start, end)} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="content-wrapper">
+				<div className="m-auto flex flex-col justify-center items-center w-full">
+					<div className="flex wrap w-full">
+						<Header />
+					</div>
+					<div className="bg-gray20 rounded-2xl wrap w-full p-4 ">
+						<div className="justify-between sm:flex">
+							<SearchInput />
+							<Dropdown />
+						</div>
+						<UsersList users={displayUser} currentUser={currentUser} />
+						<Pagination records={records} onClick={(start, end) => handlePagination(start, end)} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Leaderboard;
