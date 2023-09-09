3502815cd4c067a6e3c96a8dd9cdfd7856fbcaea
Ali Maktabi <alimaktabi55@yahoo.com>
2023-09-09 20:08:21 +0330

    added pagination and apis

--- src/pages/leaderboard/components/Pagination/pagination.tsx
+++ src/pages/leaderboard/components/Pagination/pagination.tsx
@@ -2,34 +2,41 @@ import React from 'react';
 import Icon from 'components/basic/Icon/Icon';
 
 interface Props {
-  records: { start: number; end: number };
-  onClick: (start: number, end: number) => void;
+	onClick: (page: number) => void;
+	next: number | null;
+	previous: number | null;
+	currentPage: number;
 }
 
-const Pagination = ({ records, onClick }: Props) => {
-  return (
-    <div className="flex justify-center items-center ">
-      <div className="flex gap-5 text-white items-center align-center mt-4">
-        <Icon
-          iconSrc="assets/images/leaderboard/arrow-left.svg"
-          width="8px"
-          height="14px"
-          hoverable
-          onClick={() => onClick(records.start - 10, records.start - 1)}
-        />
-        <p className="text-[14px]">
-          [{records.start}-{records.end}]
-        </p>
-        <Icon
-          iconSrc="assets/images/leaderboard/arrow-right.svg"
-          width="8px"
-          height="14px"
-          hoverable
-          onClick={() => onClick(records.start + 10, records.end + 10)}
-        />
-      </div>
-    </div>
-  );
+const Pagination = ({ onClick, next, previous, currentPage }: Props) => {
+	return (
+		<div className="flex justify-center items-center ">
+			<div className="flex gap-5 text-white items-center align-center mt-4">
+				{previous !== null && (
+					<Icon
+						iconSrc="assets/images/leaderboard/arrow-left.svg"
+						width="8px"
+						height="14px"
+						hoverable
+						onClick={() => onClick(currentPage - 1)}
+					/>
+				)}
+
+				<p className="text-[14px]">
+					[{(currentPage - 1) * 10} - {currentPage * 10}]
+				</p>
+				{next !== null && (
+					<Icon
+						iconSrc="assets/images/leaderboard/arrow-right.svg"
+						width="8px"
+						height="14px"
+						hoverable
+						onClick={() => onClick(currentPage + 1)}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Pagination;
--- src/pages/leaderboard/components/UsersList/usersList.tsx
+++ src/pages/leaderboard/components/UsersList/usersList.tsx
@@ -4,31 +4,32 @@ import UsersCard from '../UsersCard/usersCard';
 import Icon from 'components/basic/Icon/Icon';
 
 interface UsersListProp {
-  users: User[];
-  currentUser: User;
+	users: User[];
+	currentUser: User;
+	page: number;
 }
 
-const UsersList = ({ users, currentUser }: UsersListProp) => {
-  let period = { first: users[0].id, last: users[users.length - 1].id };
-  return (
-    <div className="users-card">
-      {currentUser.id < period.first && (
-        <div className="pt-4">
-          <UsersCard key={currentUser.id} user={currentUser} currentUser={currentUser} />
-          <Icon className="pt-3" iconSrc="assets/images/leaderboard/space.svg" width="28px" height="28px" />
-        </div>
-      )}
-      {users.map((user) => (
-        <UsersCard key={user.id} user={user} currentUser={currentUser} />
-      ))}
-      {currentUser.id > period.last && (
-        <div className="pt-4">
-          <Icon iconSrc="assets/images/leaderboard/space.svg" width="28px" height="28px" />
-          <UsersCard key={currentUser.id} user={currentUser} currentUser={currentUser} />
-        </div>
-      )}
-    </div>
-  );
+const UsersList = ({ users, currentUser, page }: UsersListProp) => {
+	let period = { first: users[0].id, last: users[users.length - 1].id };
+	return (
+		<div className="users-card">
+			{currentUser.id < period.first && (
+				<div className="pt-4">
+					<UsersCard index={-1} key={currentUser.id} user={currentUser} currentUser={currentUser} />
+					<Icon className="pt-3" iconSrc="assets/images/leaderboard/space.svg" width="28px" height="28px" />
+				</div>
+			)}
+			{users.map((user, index) => (
+				<UsersCard index={(page - 1) * 10 + index + 1} key={index} user={user} currentUser={currentUser} />
+			))}
+			{currentUser.id > period.last && (
+				<div className="pt-4">
+					<Icon iconSrc="assets/images/leaderboard/space.svg" width="28px" height="28px" />
+					<UsersCard index={-1} key={currentUser.id} user={currentUser} currentUser={currentUser} />
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default UsersList;
