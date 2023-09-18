be931167f5ea64a7fe4a6d8dfb0fa599f8e8ebec
Ali Maktabi <alimaktabi55@yahoo.com>
2023-09-18 21:24:45 +0330

    added beta button

--- src/pages/prize-tap/components/Header/header.tsx
+++ src/pages/prize-tap/components/Header/header.tsx
@@ -18,8 +18,14 @@ const Header = () => {
 	return (
 		<div className="header prize-tap-header-bg w-full h-[194px] bg-gray20 rounded-2xl flex justify-between items-end overflow-hidden p-4 mb-6 border-3 border-gray30">
 			<div className="header__left items-center h-auto">
-				<span className="header__left__type-logo flex mb-3 gap-3">
+				<span className="header__left__type-logo items-center flex mb-3 gap-3">
 					<img className="h-12 w-auto" src="assets/images/prize-tap/header-typography.png" />
+					<div>
+						<div className="bg-gray10 px-3 py-2 border font-bold border-gray50 text-white text-xs rounded-lg">
+							<p className="text-gradient-primary">Beta</p>
+						</div>
+					</div>
+
 					{/* <img className="h-12 w-auto" src="assets/images/prize-tap/header-logo.svg" /> */}
 				</span>
 			</div>
