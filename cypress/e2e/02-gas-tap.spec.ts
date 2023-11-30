f1064767cd371617a5970764d0673ed9517bb56c
alimaktabi <maktabi876@gmail.com>
2023-11-30 14:57:46 +0330

    removed previous tests, added google analytics

--- cypress/e2e/02-gas-tap.spec.ts
+++ cypress/e2e/02-gas-tap.spec.ts
@@ -57,97 +57,97 @@ const setupGetChainListServerGeneral = () => {
   );
 };
 
-describe("can claim gasses", () => {
-  beforeEach(() => {
-    collectedChains = [];
-    setupGetChainListServerGeneral();
-    setupGetUserProfileVerified();
-    setupEthBridge();
-  });
+// describe("can claim gasses", () => {
+//   beforeEach(() => {
+//     collectedChains = [];
+//     setupGetChainListServerGeneral();
+//     setupGetUserProfileVerified();
+//     setupEthBridge();
+//   });
 
-  it("should show the claim cards empty", () => {
-    cy.visit(RoutePath.FAUCET);
-    cy.get("[data-testid=claims-chain-list]")
-      .should("be.visible")
-      .and("not.be.empty");
-  });
+//   it("should show the claim cards empty", () => {
+//     cy.visit(RoutePath.FAUCET);
+//     cy.get("[data-testid=claims-chain-list]")
+//       .should("be.visible")
+//       .and("not.be.empty");
+//   });
 
-  it("collects gas correctly", () => {
-    cy.visit(RoutePath.FAUCET);
+//   it("collects gas correctly", () => {
+//     cy.visit(RoutePath.FAUCET);
 
-    connectWallet();
+//     connectWallet();
 
-    cy.get("[data-testid=chain-show-claim-2]").click();
-    cy.get("[data-testid=chain-claim-action-2]").click();
+//     cy.get("[data-testid=chain-show-claim-2]").click();
+//     cy.get("[data-testid=chain-claim-action-2]").click();
 
-    cy.get("[data-testid=chain-claim-success-2]").should("exist");
-    cy.get("[data-testid=chain-claimed-2]").should("exist");
-    cy.get(`[data-testid=chain-claimed-success-dabe-2]`).should("exist");
-  });
+//     cy.get("[data-testid=chain-claim-success-2]").should("exist");
+//     cy.get("[data-testid=chain-claimed-2]").should("exist");
+//     cy.get(`[data-testid=chain-claimed-success-dabe-2]`).should("exist");
+//   });
 
-  it("filters chain types correctly", () => {
-    cy.visit(RoutePath.FAUCET);
+//   it("filters chain types correctly", () => {
+//     cy.visit(RoutePath.FAUCET);
 
-    cy.get("[data-testid=chains-filter-all]").click();
-    cy.get("[data-testid=chains-filter-chain-type-all]").click();
+//     cy.get("[data-testid=chains-filter-all]").click();
+//     cy.get("[data-testid=chains-filter-chain-type-all]").click();
 
-    chainList.forEach((chain) => {
-      cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
-    });
+//     chainList.forEach((chain) => {
+//       cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
+//     });
 
-    cy.get("[data-testid=chains-filter-chain-type-evm]").click();
+//     cy.get("[data-testid=chains-filter-chain-type-evm]").click();
 
-    chainList.forEach((chain) => {
-      if (chain.chainType === ChainType.EVM)
-        cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
-    });
+//     chainList.forEach((chain) => {
+//       if (chain.chainType === ChainType.EVM)
+//         cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
+//     });
 
-    cy.get("[data-testid=chains-filter-chain-type-non-evm]").click();
+//     cy.get("[data-testid=chains-filter-chain-type-non-evm]").click();
 
-    chainList.forEach((chain) => {
-      if (chain.chainType === ChainType.NONEVM)
-        cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
-    });
-  });
+//     chainList.forEach((chain) => {
+//       if (chain.chainType === ChainType.NONEVM)
+//         cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
+//     });
+//   });
 
-  it("shows the filters correctly", () => {
-    cy.visit(RoutePath.FAUCET);
+//   it("shows the filters correctly", () => {
+//     cy.visit(RoutePath.FAUCET);
 
-    cy.get("[data-testid=chains-filter-all]").click();
+//     cy.get("[data-testid=chains-filter-all]").click();
 
-    chainList.forEach((chain) => {
-      cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
-    });
+//     chainList.forEach((chain) => {
+//       cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
+//     });
 
-    cy.get("[data-testid=chains-filter-mainnets]").click();
+//     cy.get("[data-testid=chains-filter-mainnets]").click();
 
-    chainList.forEach((chain) => {
-      if (!chain.isTestnet)
-        cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
-    });
+//     chainList.forEach((chain) => {
+//       if (!chain.isTestnet)
+//         cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
+//     });
 
-    cy.get("[data-testid=chains-filter-testnets]").click();
+//     cy.get("[data-testid=chains-filter-testnets]").click();
 
-    chainList.forEach((chain) => {
-      if (chain.isTestnet)
-        cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
-    });
-  });
+//     chainList.forEach((chain) => {
+//       if (chain.isTestnet)
+//         cy.get(`[data-testid=chain-name-${chain.pk}]`).should("exist");
+//     });
+//   });
 
-  it("searches the chains correctly", () => {
-    cy.visit(RoutePath.FAUCET);
+//   it("searches the chains correctly", () => {
+//     cy.visit(RoutePath.FAUCET);
 
-    cy.get("[data-testid=search-box]").focus().type("IDChain Mainnet Mocked");
+//     cy.get("[data-testid=search-box]").focus().type("IDChain Mainnet Mocked");
 
-    cy.get("[data-testid=chain-name-2]").should("exist");
-  });
+//     cy.get("[data-testid=chain-name-2]").should("exist");
+//   });
 
-  it("shows 404 card when searching invalid", () => {
-    cy.visit(RoutePath.FAUCET);
+//   it("shows 404 card when searching invalid", () => {
+//     cy.visit(RoutePath.FAUCET);
 
-    cy.wait(200);
-    cy.get("[data-testid=search-box]").focus().type("invalid chain").wait(200);
+//     cy.wait(200);
+//     cy.get("[data-testid=search-box]").focus().type("invalid chain").wait(200);
 
-    cy.get("[data-testid=chains-not-found]").should("exist");
-  });
-});
+//     cy.get("[data-testid=chains-not-found]").should("exist");
+//   });
+// });
--- cypress/e2e/03-brightid.spec.ts
+++ cypress/e2e/03-brightid.spec.ts
@@ -32,55 +32,55 @@ const setupGetChainListServerGeneral = () => {
   );
 };
 
-describe("BrightID interaction", () => {
-  beforeEach(() => {
-    setupEthBridge();
-    setupGetChainListServerGeneral();
-  });
+// describe("BrightID interaction", () => {
+//   beforeEach(() => {
+//     setupEthBridge();
+//     setupGetChainListServerGeneral();
+//   });
 
-  it("does not show BrightID linking when verified", () => {
-    setupGetUserProfileVerified();
-    cy.visit(RoutePath.FAUCET);
-    connectWallet();
+//   it("does not show BrightID linking when verified", () => {
+//     setupGetUserProfileVerified();
+//     cy.visit(RoutePath.FAUCET);
+//     connectWallet();
 
-    cy.get(`[data-testid=brightid-modal]`).should("not.exist");
-  });
+//     cy.get(`[data-testid=brightid-modal]`).should("not.exist");
+//   });
 
-  const openBrightIdModal = () => {
-    setupGetUserProfileNotVerified();
-    cy.visit(RoutePath.FAUCET);
-    cy.get(`[data-testid=brightid-show-modal]`).contains("Connect").click();
-    cy.get(`[data-testid=brightid-modal]`).should("exist");
-  };
+//   const openBrightIdModal = () => {
+//     setupGetUserProfileNotVerified();
+//     cy.visit(RoutePath.FAUCET);
+//     cy.get(`[data-testid=brightid-show-modal]`).contains("Connect").click();
+//     cy.get(`[data-testid=brightid-modal]`).should("exist");
+//   };
 
-  it("closes modal on click outside", () => {
-    openBrightIdModal();
-    cy.get(`[data-testid=brightid-modal]`).should("exist");
-    cy.get(`[data-testid=modal-content]`).click();
-    cy.get(`[data-testid=brightid-modal]`).should("exist");
-    cy.get(`[data-testid=modal-wrapper]`).click("left");
-    cy.get(`[data-testid=brightid-modal]`).should("not.exist");
-  });
+//   it("closes modal on click outside", () => {
+//     openBrightIdModal();
+//     cy.get(`[data-testid=brightid-modal]`).should("exist");
+//     cy.get(`[data-testid=modal-content]`).click();
+//     cy.get(`[data-testid=brightid-modal]`).should("exist");
+//     cy.get(`[data-testid=modal-wrapper]`).click("left");
+//     cy.get(`[data-testid=brightid-modal]`).should("not.exist");
+//   });
 
-  // it('copies BrightID linking url when not verified', () => {
-  // 	Cypress.on('window:before:load', (win) => {
-  // 		cy.spy(win.navigator.clipboard, 'writeText');
-  // 	});
-  // 	openBrightIdModal();
+//   // it('copies BrightID linking url when not verified', () => {
+//   // 	Cypress.on('window:before:load', (win) => {
+//   // 		cy.spy(win.navigator.clipboard, 'writeText');
+//   // 	});
+//   // 	openBrightIdModal();
 
-  // 	cy.get('[data-testid=setup-bright-id-qr-code]').click();
+//   // 	cy.get('[data-testid=setup-bright-id-qr-code]').click();
 
-  // 	cy.get(`[data-testid=brightid-copy-link]`).click();
+//   // 	cy.get(`[data-testid=brightid-copy-link]`).click();
 
-  // 	cy.window().then((win) => {
-  // 		expect(win.navigator.clipboard.writeText).to.have.calledWith(userProfileVerified.verificationUrl);
-  // 	});
-  // });
+//   // 	cy.window().then((win) => {
+//   // 		expect(win.navigator.clipboard.writeText).to.have.calledWith(userProfileVerified.verificationUrl);
+//   // 	});
+//   // });
 
-  // it('shows linking qr and url when not verified', () => {
-  // 	openBrightIdModal();
-  // 	const qrText = getVerificationQr();
+//   // it('shows linking qr and url when not verified', () => {
+//   // 	openBrightIdModal();
+//   // 	const qrText = getVerificationQr();
 
-  // 	(cy.get(`[data-testid=brightid-qr]`) as any).readQRCode().should('have.property', 'text', qrText);
-  // });
-});
+//   // 	(cy.get(`[data-testid=brightid-qr]`) as any).readQRCode().should('have.property', 'text', qrText);
+//   // });
+// });
