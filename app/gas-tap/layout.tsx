4aa8a5b22c29242cec8e9e2879e936d948c17a8b
Ali Maktabi <67747298+alimaktabi@users.noreply.github.com>
2023-12-02 21:16:48 +0330

    fixed cache reset after 10 seconds

--- app/gas-tap/layout.tsx
+++ app/gas-tap/layout.tsx
@@ -2,9 +2,11 @@ import GasTapProvider from "@/context/gasTapProvider";
 import { FC, PropsWithChildren } from "react";
 
 const GasTapLayout: FC<PropsWithChildren> = async ({ children }) => {
-  const chains = await fetch(
-    process.env.API_URL! + "/api/gastap/chain/list/"
-  ).then((res) => res.json());
+  const chains = await fetch(process.env.API_URL! + "/api/gastap/chain/list/", {
+    next: {
+      revalidate: 10,
+    },
+  }).then((res) => res.json());
 
   return <GasTapProvider chains={chains}>{children}</GasTapProvider>;
 };
