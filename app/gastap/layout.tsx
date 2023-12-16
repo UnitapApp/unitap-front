a3fd030c2a7e1d280ee27cc0eb1399991b23cdc0
Ali Maktabi <67747298+alimaktabi@users.noreply.github.com>
2023-12-17 03:27:43 +0330

    fixed build error

--- app/gastap/layout.tsx
+++ app/gastap/layout.tsx
@@ -3,14 +3,17 @@ import { Chain } from "@/types";
 import { FC, PropsWithChildren } from "react";
 
 const GasTapLayout: FC<PropsWithChildren> = async ({ children }) => {
-  const chains = (
-    await fetch(process.env.NEXT_PUBLIC_API_URL! + "/api/gastap/chain/list/", {
+  const chainsApi = await fetch(
+    process.env.NEXT_PUBLIC_API_URL! + "/api/gastap/chain/list/",
+    {
       next: {
         revalidate: 10,
       },
       cache: "no-cache",
-    }).then((res) => res.json())
-  ).toSorted((a: Chain, b: Chain) =>
+    }
+  ).then((res) => res.json());
+
+  const chains = chainsApi.toSorted((a: Chain, b: Chain) =>
     a.isOneTimeClaim && b.isOneTimeClaim
       ? 0
       : a.isOneTimeClaim
