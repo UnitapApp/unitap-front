1478d87110c36bbf06d3cbdc249dbff2af58f14d
Ali Maktabi <67747298+alimaktabi@users.noreply.github.com>
2023-12-17 20:18:55 +0330

    fixed namings and cache

--- app/gastap/layout.tsx
+++ app/gastap/layout.tsx
@@ -12,9 +12,6 @@ const GasTapLayout: FC<PropsWithChildren> = async ({ children }) => {
   const chainsApi = await fetch(
     process.env.NEXT_PUBLIC_API_URL! + "/api/gastap/chain/list/",
     {
-      next: {
-        revalidate: 10,
-      },
       cache: "no-store",
     }
   ).then((res) => res.json());
@@ -23,9 +20,11 @@ const GasTapLayout: FC<PropsWithChildren> = async ({ children }) => {
 
   const token = cookieStore.get("userToken");
 
-  const claimedChains = await getOneTimeClaimedReceiptsServer(token?.value);
+  const oneTimeClaimedChains = await getOneTimeClaimedReceiptsServer(
+    token?.value
+  );
 
-  const oneTimeClaimedChains = await getClaimedReceiptsServer(token?.value);
+  const claimedChains = await getClaimedReceiptsServer(token?.value);
 
   const chains = chainsApi as Array<Chain>;
 
