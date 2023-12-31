a527ee0c47d8fd5d75209f5174f57cd9be34a333
abbasnosrati <abbas.nosrati68@gmail.com>
2023-12-31 17:48:09 +0330

    fixed bug

--- utils/serverApis/prizetap.ts
+++ utils/serverApis/prizetap.ts
@@ -1,7 +1,8 @@
 import { Prize } from "@/types";
 
 export async function getRafflesServerSideListAPI() {
-  const response: Prize[] = await fetch("/api/prizetap/raffle-list/", {
+  const response: Prize[] = await fetch(
+    process.env.NEXT_PUBLIC_API_URL +"/api/prizetap/raffle-list/", {
     cache: "no-store",
   }).then((res) => res.json());
 
