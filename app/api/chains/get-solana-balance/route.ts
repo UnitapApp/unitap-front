662695508ab2ce9d647cffcacf1d0b3476222ead
alimaktabi <maktabi876@gmail.com>
2024-01-01 18:49:29 +0330

    fixed reading solana addresses from chainlist api

--- app/api/chains/get-solana-balance/route.ts
+++ app/api/chains/get-solana-balance/route.ts
@@ -1,31 +1,50 @@
-import { NextResponse } from "next/server"
-import { Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js"
+import { NextResponse } from "next/server";
+import { Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { getChainList } from "@/utils/api";
+import { ChainType } from "@/types";
 
-const SOLANA_CONNECTION = new Connection(process.env.SOLANA_HTTP_PROVIDER!)
+const SOLANA_CONNECTION = new Connection(process.env.SOLANA_HTTP_PROVIDER!);
 
-const WALLET_ADDRESS = process.env.SOLANA_WALLET_ADDRESS!
+const SOLANA_CHAIN_ID = "1399811149";
 
-export const revalidate = true
-export const maxDuration = 10
+export const revalidate = true;
+export const maxDuration = 10;
 
 export async function GET() {
+  const chains = await getChainList();
+
+  const solanaChain = chains.find(
+    (chain) =>
+      chain.chainType === ChainType.SOLANA &&
+      !chain.isTestnet &&
+      chain.chainId === SOLANA_CHAIN_ID
+  );
+
+  if (!solanaChain)
+    return NextResponse.json(
+      {
+        balance: null,
+      },
+      { status: 200 }
+    );
+
   try {
     const balance = await SOLANA_CONNECTION.getBalance(
-      new PublicKey(WALLET_ADDRESS)
-    )
+      new PublicKey(solanaChain?.fundManagerAddress)
+    );
 
     return NextResponse.json(
       { balance: balance / LAMPORTS_PER_SOL },
       { status: 200 }
-    )
+    );
   } catch (e) {
-    console.error(e)
+    console.error(e);
 
     return NextResponse.json(
       {
         balance: null,
       },
       { status: 200 }
-    )
+    );
   }
 }
--- app/api/chains/get-solana-testnet-balance/route.ts
+++ app/api/chains/get-solana-testnet-balance/route.ts
@@ -1,36 +1,57 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 import {
   Connection,
   PublicKey,
   LAMPORTS_PER_SOL,
   clusterApiUrl,
-} from "@solana/web3.js"
+} from "@solana/web3.js";
+import { getChainList } from "@/utils/api";
+import { ChainType } from "@/types";
 
-const SOLANA_CONNECTION = new Connection(clusterApiUrl("testnet"))
+const SOLANA_CONNECTION = new Connection(clusterApiUrl("testnet"));
 
-const WALLET_ADDRESS = process.env.SOLANA_TESTNET_WALLET_ADDRESS!
+const WALLET_ADDRESS = process.env.SOLANA_TESTNET_WALLET_ADDRESS!;
 
-export const revalidate = true
-export const maxDuration = 10
+const SOLANA_TESTNET_CHAIN_ID = "1399811150";
+
+export const revalidate = true;
+export const maxDuration = 10;
 
 export async function GET() {
+  const chains = await getChainList();
+
+  const solanaChain = chains.find(
+    (chain) =>
+      chain.chainType === ChainType.SOLANA &&
+      chain.isTestnet &&
+      chain.chainId === SOLANA_TESTNET_CHAIN_ID
+  );
+
+  if (!solanaChain)
+    return NextResponse.json(
+      {
+        balance: null,
+      },
+      { status: 200 }
+    );
+
   try {
     const balance = await SOLANA_CONNECTION.getBalance(
-      new PublicKey(WALLET_ADDRESS)
-    )
+      new PublicKey(solanaChain?.fundManagerAddress)
+    );
 
     return NextResponse.json(
       { balance: balance / LAMPORTS_PER_SOL },
       { status: 200 }
-    )
+    );
   } catch (e) {
-    console.error(e)
+    console.error(e);
 
     return NextResponse.json(
       {
         balance: null,
       },
       { status: 200 }
-    )
+    );
   }
 }
