228a624df2b947ee680d63e69a5b3c38266dce1a
abbasnosrati <abbas.nosrati68@gmail.com>
2024-01-08 16:36:44 +0330

    add new routes

--- utils/routes.ts
+++ utils/routes.ts
@@ -11,6 +11,9 @@ enum RoutePath {
   PROVIDER_GASTAP = "/contribution-hub/gas-tap",
   PROVIDER_TOKENTAP = "/contribution-hub/token-tap",
   PROVIDER_PRIZETAP = "/contribution-hub/prize-tap",
+  PROVIDER_PRIZETAP_CREATE = "/contribution-hub/prize-tap/create",
+  PROVIDER_PRIZETAP_DETAILS = "/contribution-hub/prize-tap/details",
+  PROVIDER_PRIZETAP_VERIFICATION = "/contribution-hub/prize-tap/verification",
 }
 
 export default RoutePath;
