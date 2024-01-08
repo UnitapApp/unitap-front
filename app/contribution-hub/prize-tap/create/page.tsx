6325c7443904babc3142a027ffb3489ffe812bea
abbasnosrati <abbas.nosrati68@gmail.com>
2024-01-08 16:24:55 +0330

    new page and layout for create raffle

---
+++ app/contribution-hub/prize-tap/create/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import OfferPrizeForm from "@/components/containers/provider-dashboard/prize-tap/OfferPrizeForm";
+
+const Page = () => {
+  return (
+    <div>
+      <OfferPrizeForm />
+    </div>
+  );
+};
+
+export default Page;
