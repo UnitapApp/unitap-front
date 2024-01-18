433ae69f4134e8ca344d65014078a59ce4761185
abbasnosrati <abbas.nosrati68@gmail.com>
2024-01-18 12:32:17 +0330

    Separate validation function

--- components/containers/provider-dashboard/helpers/isValidContractAddress.ts
+++ components/containers/provider-dashboard/helpers/isValidContractAddress.ts
@@ -6,6 +6,7 @@ export const isValidContractAddress =
 			const res = await provider?.getBytecode({
 				address: contractAddress as any,
 			});
+			if(res == undefined) return false
 			return res != "0x";
 		} catch {
 			return false;
