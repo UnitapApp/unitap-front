71aabaa159fe3859204ee3270e55b7c63328962b
abbasnosrati <abbas.nosrati68@gmail.com>
2024-01-20 09:46:06 +0330

    get user Distribution token tap

--- utils/serverApis/contributionHub.ts
+++ utils/serverApis/contributionHub.ts
@@ -17,7 +17,38 @@ export const getUserRaffleListServerSide = async (token?: string) => {
   return res as UserRafflesProps[];
 };
 
+
+export const getUserDistributionListServerSide = async (token?: string) => {
+  if (!token) return [];
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API_URL! + "/api/tokentap/token-distribution-list/",
+    {
+      cache: "no-store",
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    }
+  ).then((res) => res.json());
+
+  if (!Array.isArray(res)) return [];
+
+  return res as UserRafflesProps[];
+};
+
 export const getConstraintListServer = async () => {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API_URL! + "/api/prizetap/get-constraints/",
+    {
+      cache: "no-store"
+    }
+  ).then((res) => res.json());
+
+  if(!Array.isArray(res)) return [];
+
+  return res as ConstraintProps[]
+}
+
+export const getTokenTapConstraintListServer = async () => {
   const res = await fetch(
     process.env.NEXT_PUBLIC_API_URL! + "/api/prizetap/get-constraints/",
     {
