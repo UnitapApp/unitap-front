c78a8791419f39ee485834186a25db2dd4616bf5
Ali Maktabi <67747298+alimaktabi@users.noreply.github.com>
2024-03-12 14:12:12 +0330

    fixed markdown on dev

--- components/containers/token-tap/Markdown.tsx
+++ components/containers/token-tap/Markdown.tsx
@@ -1,15 +1,22 @@
-import ReactMarkdown from 'react-markdown';
+import { FC } from "react";
+import ReactMarkdown from "react-markdown";
 
-const Markdown = ({ content, isHighlighted }: { content: string; isHighlighted?: boolean }) => {
-	return (
-		<ReactMarkdown
-			className={`markdown text-xs text-gray100 leading-7 pl-6 md:pl-16 pt-4 pr-6 text-justify pb-10 ${
-				isHighlighted ? 'bg-g-primary-low' : 'bg-gray40'
-			}`}
-		>
-			{content}
-		</ReactMarkdown>
-	);
+export type MarkdownProps = {
+  content: string;
+  isHighlighted?: boolean;
+  className?: string;
+};
+
+const Markdown: FC<MarkdownProps> = ({ content, isHighlighted, className }) => {
+  return (
+    <ReactMarkdown
+      className={`markdown text-xs ${className} text-gray100 leading-7 pl-6 md:pl-16 pt-4 pr-6 text-justify pb-10 ${
+        isHighlighted ? "bg-g-primary-low" : "bg-gray40"
+      }`}
+    >
+      {content}
+    </ReactMarkdown>
+  );
 };
 
 export default Markdown;
