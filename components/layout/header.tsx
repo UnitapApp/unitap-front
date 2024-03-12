558a22f3c57a7c38ef6234d158ee49bec97414a4
alimaktabi <maktabi876@gmail.com>
2024-03-12 14:40:03 +0330

    fixed prettier
    added plugin prettier-tailwind to align the classnames

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -9,7 +9,7 @@ const UserAuthStatus = dynamic(() => import("./auth"), { ssr: false });
 
 const Header = () => {
   return (
-    <header className="text-xs w-full flex items-center top-0 z-20 bg-gray10 py-3 px-8">
+    <header className="top-0 z-20 flex w-full items-center bg-gray10 px-8 py-3 text-xs">
       <Link href={RoutePath.HOME}>
         <Image
           src="/assets/images/navbar/logo.svg"
@@ -21,7 +21,7 @@ const Header = () => {
 
       <UserAuthStatus />
 
-      <div className="hidden md:flex flex-1"></div>
+      <div className="hidden flex-1 md:flex"></div>
 
       <div className="hidden md:flex">
         <RenderNavbarDropdown />
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: "./tailwind.config.ts",
+};
+
