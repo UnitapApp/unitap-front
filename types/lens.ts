f07dd2ece379e61457e58c2e0243fa6f71f53013
Ali Maktabi <67747298+alimaktabi@users.noreply.github.com>
2024-03-14 18:20:44 +0330

    added farcaster user input

--- types/lens.ts
+++ types/lens.ts
@@ -14,3 +14,19 @@ export type LensUserProfile = {
     } | null;
   } | null;
 };
+
+export type FarcasterProfile = {
+  fid: number;
+  custody_address: string;
+  username: string;
+  pfp_url: string;
+  profile: {
+    bio: {
+      text: string;
+    };
+  };
+
+  follower_count: number;
+  following_count: number;
+  active_status: string;
+};
