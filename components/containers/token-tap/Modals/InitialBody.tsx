81c3bd107e0125010377599476e65e1345dd9ad3
abbasnosrati <abbas.nosrati68@gmail.com>
2024-05-08 09:50:55 +0330

    check network on claiming token after get signature

--- components/containers/token-tap/Modals/InitialBody.tsx
+++ components/containers/token-tap/Modals/InitialBody.tsx
@@ -3,17 +3,21 @@ import Icon from "@/components/ui/Icon";
 import { shortenAddress } from "@/utils";
 import Link from "next/link";
 import { Text } from "@/components/ui/text.style";
-import { useWalletAccount } from "@/utils/wallet";
+import { useWalletAccount, useWalletNetwork } from "@/utils/wallet";
 import { FC } from "react";
 import { Token } from "@/types";
 import { useTokenTapContext } from "@/context/tokenTapProvider";
 import { DropIconWrapper } from "../../modals/claimModal.style";
 import WalletAddress from "../../../../app/prizetap/components/Modals/wallet-address";
+import WrongNetworkBody from "./WrongNetworkBody";
 
 const InitialBody: FC<{
   token: Token;
 }> = ({ token }) => {
   const { isConnected, address } = useWalletAccount();
+  const { chain: activatedChain } = useWalletNetwork();
+
+  const chainId = activatedChain?.id;
 
   const {
     handleClaimToken,
@@ -26,84 +30,94 @@ const InitialBody: FC<{
     token.amount / 10 ** (token.decimals ?? token.chain.decimals);
 
   return (
-    <>
-      <DropIconWrapper data-testid={`chain-claim-initial-${token.chain.pk}`}>
-        <Icon
-          className="chain-logo z-10 mb-10 mt-14"
-          width="auto"
-          height="110px"
-          iconSrc={token.imageUrl}
-          alt=""
+    <div>
+      {chainId?.toString() !== token?.chain.chainId ?
+
+        <WrongNetworkBody
+          chain={token.chain}
+          imageUrl={token.imageUrl}
         />
-      </DropIconWrapper>
-      {claimTokenResponse?.state === "Retry" ? (
-        <p className="my-4 mb-6 px-3 text-center text-sm text-warn">
-          {claimTokenResponse?.message}
-        </p>
-      ) : (
-        <div className="text-left text-white">
-          {claimTokenSignatureLoading ? (
-            <p className="my-4 mb-6 px-3 text-center text-sm text-white">
-              Preparing your Enroll signature...
-            </p>
-          ) : claimTokenResponse?.state === "Retry" ? (
-            <p className="my-4 mb-6 px-3 text-center text-sm text-error">
+        :
+
+        <>
+          <DropIconWrapper data-testid={`chain-claim-initial-${token.chain.pk}`}>
+            <Icon
+              className="chain-logo z-10 mb-10 mt-14"
+              width="auto"
+              height="110px"
+              iconSrc={token.imageUrl}
+              alt=""
+            />
+          </DropIconWrapper>
+          {claimTokenResponse?.state === "Retry" ? (
+            <p className="my-4 mb-6 px-3 text-center text-sm text-warn">
               {claimTokenResponse?.message}
             </p>
-          ) : claimTokenLoading ? (
-            <p className="mb-2 text-center text-lg leading-loose">
-              Your claim is ready.
-            </p>
           ) : (
-            <p></p>
+            <div className="text-left text-white">
+              {claimTokenSignatureLoading ? (
+                <p className="my-4 mb-6 px-3 text-center text-sm text-white">
+                  Preparing your Enroll signature...
+                </p>
+              ) : claimTokenResponse?.state === "Retry" ? (
+                <p className="my-4 mb-6 px-3 text-center text-sm text-error">
+                  {claimTokenResponse?.message}
+                </p>
+              ) : claimTokenLoading ? (
+                <p className="mb-2 text-center text-lg leading-loose">
+                  Your claim is ready.
+                </p>
+              ) : (
+                <p></p>
+              )}
+
+              <p className="mb-2 text-xs">
+                If you have not already claimed your tokens, you can claim them now.
+              </p>
+              <p className="mb-2 text-xs">
+                You will need to sign a wallet transaction and pay a small gas fee
+                to claim tokens.
+              </p>
+              <p className="mb-6 text-xs">
+                If you do not have sufficient gas, please visit{" "}
+                <Link
+                  className="text-blue-500"
+                  href={"/gastap?hc=" + token.chain.chainName}
+                >
+                  Gas Tap
+                </Link>
+                .
+              </p>
+            </div>
           )}
 
-          <p className="mb-2 text-xs">
-            If you have not already claimed your tokens, you can claim them now.
-          </p>
-          <p className="mb-2 text-xs">
-            You will need to sign a wallet transaction and pay a small gas fee
-            to claim tokens.
-          </p>
-          <p className="mb-6 text-xs">
-            If you do not have sufficient gas, please visit{" "}
-            <Link
-              className="text-blue-500"
-              href={"/gastap?hc=" + token.chain.chainName}
-            >
-              Gas Tap
-            </Link>
-            .
-          </p>
-        </div>
-      )}
-
-      <Text width="100%" fontSize="14">
-        Wallet Address
-      </Text>
-      <WalletAddress fontSize="12">
-        {isConnected ? shortenAddress(address) : ""}
-      </WalletAddress>
-
-      <ClaimButton
-        onClick={handleClaimToken}
-        $width="100%"
-        $fontSize="16px"
-        disabled={claimTokenSignatureLoading || claimTokenLoading}
-        className="!w-full"
-        data-testid={`token-claim-action-${token.chain.pk}`}
-      >
-        {claimTokenLoading ? (
-          <p>Claiming...</p>
-        ) : claimTokenSignatureLoading ? (
-          <p>Preparing Signature...</p>
-        ) : claimTokenResponse?.state === "Retry" ? (
-          <p>Retry</p>
-        ) : (
-          <p>{`Claim ${calculateClaimAmount} ${token.token}`}</p>
-        )}
-      </ClaimButton>
-    </>
+          <Text width="100%" fontSize="14">
+            Wallet Address
+          </Text>
+          <WalletAddress fontSize="12">
+            {isConnected ? shortenAddress(address) : ""}
+          </WalletAddress>
+          <ClaimButton
+            onClick={handleClaimToken}
+            $width="100%"
+            $fontSize="16px"
+            disabled={claimTokenSignatureLoading || claimTokenLoading}
+            className="!w-full"
+            data-testid={`token-claim-action-${token.chain.pk}`}
+          >
+            {claimTokenLoading ? (
+              <p>Claiming...</p>
+            ) : claimTokenSignatureLoading ? (
+              <p>Preparing Signature...</p>
+            ) : claimTokenResponse?.state === "Retry" ? (
+              <p>Retry</p>
+            ) : (
+              <p>{`Claim ${calculateClaimAmount} ${token.token}`}</p>
+            )}
+          </ClaimButton>
+        </>
+      }
+    </div>
   );
 };
 
