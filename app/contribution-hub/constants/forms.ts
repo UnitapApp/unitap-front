597a7c59e1c58fdf36c4dda8f30c596caa237f29
abbasnosrati <abbas.nosrati68@gmail.com>
2024-06-18 11:23:52 +0330

    bring requirement to first page

--- app/contribution-hub/constants/forms.ts
+++ app/contribution-hub/constants/forms.ts
@@ -13,9 +13,9 @@ import TokenContactInformation from "../token-tap/components/OfferTokenForm/Toke
 import TokenInformationVerification from "../token-tap/components/OfferTokenForm/TokenInformationVerification";
 
 export const tokenTapForms = [
+  TokenTapRequirements,
   TokenInfo,
   TokenTapTimeEnrollLimitation,
-  TokenTapRequirements,
   TokenContactInformation,
   DepositToken,
   TokenInformationVerification,
@@ -24,6 +24,14 @@ export const tokenTapForms = [
 export const TokenTapDisplaySteps: DisplayStepsProps[] = [
   {
     id: 0,
+    title: "Requirements",
+    description: "Add requirements for Enrollment",
+    prevIcon: "/assets/images/provider-dashboard/step-2-green.png",
+    activeIcon: "/assets/images/provider-dashboard/step-2-active.png",
+    nextIcon: "/assets/images/provider-dashboard/step-2-off.png",
+  },
+  {
+    id: 1,
     title: "Token info",
     description: "Your Token information",
     prevIcon: "/assets/images/provider-dashboard/token-tap-green.svg",
@@ -31,20 +39,12 @@ export const TokenTapDisplaySteps: DisplayStepsProps[] = [
     nextIcon: "/assets/images/provider-dashboard/token-tap-active.png",
   },
   {
-    id: 1,
+    id: 2,
     title: "Time/Claim Limitation",
     description: "Information of time and Claims ",
     prevIcon: "/assets/images/provider-dashboard/step-1-green.svg",
     activeIcon: "/assets/images/provider-dashboard/step-1-active.svg",
     nextIcon: "/assets/images/provider-dashboard/step-1-off.svg",
-  },
-  {
-    id: 2,
-    title: "Requirements",
-    description: "Add requirements for Enrollment",
-    prevIcon: "/assets/images/provider-dashboard/step-2-green.png",
-    activeIcon: "/assets/images/provider-dashboard/step-2-active.png",
-    nextIcon: "/assets/images/provider-dashboard/step-2-off.png",
   },
   {
     id: 3,
@@ -73,9 +73,9 @@ export const TokenTapDisplaySteps: DisplayStepsProps[] = [
 ];
 
 export const prizeTapForms = [
+  Requirements,
   PrizeInfo,
   TimeEnrollLimitation,
-  Requirements,
   ContactInformation,
   DepositPrize,
   InformationVerification,
@@ -84,6 +84,14 @@ export const prizeTapForms = [
 export const PrizeTapDisplaySteps: DisplayStepsProps[] = [
   {
     id: 0,
+    prevIcon: "/assets/images/provider-dashboard/step-2-green.png",
+    activeIcon: "/assets/images/provider-dashboard/step-2-active.png",
+    nextIcon: "/assets/images/provider-dashboard/step-2-off.png",
+    title: "Requirements",
+    description: "Set enrollment requirements",
+  },
+  {
+    id: 1,
     prevIcon:
       "/assets/images/provider-dashboard/prizerForm-step-diamond-green.svg",
     activeIcon: "/assets/images/provider-dashboard/prizeForm-step-diamond.png",
@@ -92,20 +100,12 @@ export const PrizeTapDisplaySteps: DisplayStepsProps[] = [
     description: "Information on your prize",
   },
   {
-    id: 1,
+    id: 2,
     prevIcon: "/assets/images/provider-dashboard/step-1-green.svg",
     activeIcon: "/assets/images/provider-dashboard/step-1-active.svg",
     nextIcon: "/assets/images/provider-dashboard/step-1-off.svg",
     title: "Time and Enrollment Limitations",
     description: "Set time and date, and enrollment rules",
-  },
-  {
-    id: 2,
-    prevIcon: "/assets/images/provider-dashboard/step-2-green.png",
-    activeIcon: "/assets/images/provider-dashboard/step-2-active.png",
-    nextIcon: "/assets/images/provider-dashboard/step-2-off.png",
-    title: "Requirements",
-    description: "Set enrollment requirements",
   },
   {
     id: 3,
