699d1917ff5b61d3dbbb4dff9df15a91ce94c047
alimaktabi <maktabi876@gmail.com>
2024-07-10 10:59:18 +0330

    added arb bridge app

--- app/contribution-hub/constants/integrations.ts
+++ app/contribution-hub/constants/integrations.ts
@@ -20,4 +20,8 @@ export const appInfos: {
     logo: "/assets/images/provider-dashboard/requirements/farcaster.svg",
     label: "Farcaster",
   },
+  arbBridge: {
+    logo: "https://imagedelivery.net/XQ6LDks1pWNDtTDAw7o9nA/03ad6a53-03e3-4aae-7746-8d24b9b5e600/public",
+    label: "Arbitrum Bridge",
+  },
 };
