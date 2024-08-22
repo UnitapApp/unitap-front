02430a01e4a1724d2d2b5da27e9894c8f2d6e099
Ali Maktabi <maktabi876@gmail.com>
2024-08-22 13:48:50 +0330

    fixed wallet connections

--- utils/wallet/wagmi.ts
+++ utils/wallet/wagmi.ts
@@ -1,17 +1,9 @@
 import { cookieStorage, createConfig, createStorage, http } from "wagmi";
 import { supportedChains } from "@/constants/chains";
 import { injected, mock, safe, walletConnect } from "wagmi/connectors";
+
 import { HttpTransport } from "viem";
 import { TEST_PRIVATE_KEY } from "@/cypress/utils/data";
-
-// export const wagmiPublicProvider = publicProvider();
-
-// export const wagmiJsonRpcProvider = jsonRpcProvider({
-//   rpc: (chain: Chain) => ({
-//     http: RPC_URLS[chain.id as SupportedChainId],
-//     webSocket: RPC_URLS_WEBSOCKET[chain.id as SupportedChainId],
-//   }),
-// });
 
 const getConnectorProviders = () => {
   if (process.env.NEXT_PUBLIC_IS_TESTING === "1") {
@@ -26,7 +18,9 @@ const getConnectorProviders = () => {
   }
 
   return [
-    injected(),
+    injected({
+      shimDisconnect: true,
+    }),
     walletConnect({
       projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
     }),
@@ -48,4 +42,5 @@ export const config = createConfig({
   chains: supportedChains as any,
   connectors: getConnectorProviders(),
   transports,
+  multiInjectedProviderDiscovery: true,
 });
