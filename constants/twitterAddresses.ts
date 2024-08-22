e54f12cb76d9307e0b17e71387682e3ffede087d
abbasnosrati <abbas.nosrati68@gmail.com>
2024-08-22 08:23:55 +0330

    sort twitter addresses

--- constants/twitterAddresses.ts
+++ constants/twitterAddresses.ts
@@ -19,6 +19,22 @@ export const FarcasterObject: Record<string, TwitterUser> = {
 };
 
 export const TwitterObject: Record<string, TwitterUser> = {
+  "1544104934274211847": {
+    name: "thriveprotocol",
+    logo: "./assets/images/twitter/Thrive_Protocol.png",
+  },
+  "1676050567842635776": {
+    name: "ThrivePolygon",
+    logo: "./assets/images/twitter/Thrive_Polygon.png",
+  },
+  "1522613392766377984": {
+    name: "Unitap_app",
+    logo: "./assets/images/twitter/logo.png",
+  },
+  "1728723404033814528": {
+    name: "0xmayalabs",
+    logo: "./assets/images/twitter/Maya_Labs.png",
+  },
   "1456524590004510731": {
     name: "3look_io",
     logo: "./assets/images/twitter/3look.png",
@@ -35,9 +51,9 @@ export const TwitterObject: Record<string, TwitterUser> = {
     name: "CarbonCreditXyz",
     logo: "./assets/images/twitter/Carbon_Credit.png",
   },
-  "1803169714245439488": {
-    name: "RedditCurrency",
-    logo: "./assets/images/twitter/Community_Currency.png",
+  "1650810785474437121": {
+    name: "citizenwallet",
+    logo: "./assets/images/twitter/Citizen_Wallet.png",
   },
   "1781331280006725632": {
     name: "crackandstack",
@@ -63,17 +79,13 @@ export const TwitterObject: Record<string, TwitterUser> = {
     name: "kleo_network",
     logo: "./assets/images/twitter/Kleo_Network.png",
   },
-  "1728723404033814528": {
-    name: "0xmayalabs",
-    logo: "./assets/images/twitter/Maya_Labs.png",
-  },
-  "1650810785474437121": {
-    name: "citizenwallet",
-    logo: "./assets/images/twitter/Citizen_Wallet.png",
-  },
   "1672990186110582784": {
     name: "rafflxyz",
     logo: "./assets/images/twitter/Raffl.png",
+  },
+  "1803169714245439488": {
+    name: "RedditCurrency",
+    logo: "./assets/images/twitter/Community_Currency.png",
   },
   "1371771575591071750": {
     name: "SmartLayer",
@@ -95,10 +107,7 @@ export const TwitterObject: Record<string, TwitterUser> = {
     name: "toasterfinance",
     logo: "./assets/images/twitter/Toaste_ Finance.png",
   },
-  "1522613392766377984": {
-    name: "Unitap_app",
-    logo: "./assets/images/twitter/logo.png",
-  },
+
   "1492148219190685697": {
     name: "UPPFIRST",
     logo: "./assets/images/twitter/UPPFIRST.png",
@@ -110,14 +119,6 @@ export const TwitterObject: Record<string, TwitterUser> = {
   "1543940184273817601": {
     name: "Xerberus_io",
     logo: "./assets/images/twitter/Xerberus.png",
-  },
-  "1405121796341334016": {
-    name: "0xPolygonEco",
-    logo: "./assets/images/twitter/polygon.png",
-  },
-  "1676050567842635776": {
-    name: "ThrivePolygon",
-    logo: "./assets/images/twitter/Thrive_Polygon.png",
   },
   "1381921058694049797": {
     name: "xionglobal",
