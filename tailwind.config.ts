4fb2a53532b5f81bdf907d76ff54ecf9915794c8
D2Mark <maktabi876@gmail.com>
2025-03-28 23:14:56 +0330

    added shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
         xl1440: "1440px",
       },
       width: {
-        68: "17rem",
-        88: "22rem",
-        100: "25rem",
-        104: "26rem",
+        "68": "17rem",
+        "88": "22rem",
+        "100": "25rem",
+        "104": "26rem",
       },
       fontSize: {
         "2xs": "0.625rem",
@@ -34,9 +34,15 @@ const config: Config = {
         "raffle-card": "#F7FFEF",
         "black-0": "#000",
         "gray-secondary": "#D9D9D926",
-        primary: "#EF476F",
-        secondary: "#F59569",
-        input: "#EFF3F8",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        input: "hsl(var(--input))",
         label: "#565656",
         black: "#353535",
         body: "#21212C",
@@ -77,6 +83,47 @@ const config: Config = {
         "secondary-text": "#997EA4",
         warn: "#F16E35",
         warning2: "#877A74",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       boxShadow: {
         "primary-button":
@@ -85,14 +132,9 @@ const config: Config = {
         "card-image": "4px 6px 0px 0px #000000",
       },
       borderWidth: {
-        3: "3px",
+        "3": "3px",
       },
       backgroundImage: {
-        "dark-primary":
-          "linear-gradient(to right, #22222f, #21232e, #242130, #231f2f)",
-        "dark-primary-2": "url('/assets/images/token-tap/bg-primary-dark.svg')",
-        // "dark-primary-2":
-        //   "radial-gradient(174.04% 3217.67% at 17.05% 37.02%, #2A3837 0%, #3F2B44 42.19%, #322837 100%)",
         primaryGradient:
           "linear-gradient(91.35deg, #4BF2A2 -4.66%, #A89FE7 39.49%, #E1C3F4 73.07%, #DD40CD 111.44%)",
         primaryGradient2:
@@ -101,30 +143,6 @@ const config: Config = {
           "linear-gradient(91.35deg, #4BF2A2 -4.66%, #A89FE7 89.49%, #E1C3F4 156.07%, #DD40CD 100.44%)",
         "primaryGradient-2":
           "linear-gradient(91.35deg, #4BF2A2 -150.66%, #A89FE7 39.49%, #E1C3F4 150.07%)",
-        "home-header-texture":
-          "url('/assets/images/landing/main-bg-profile.svg')",
-        "what-is-unitap-header":
-          "url('/assets/images/about/what-is-unitap-header.png')",
-        "gastap-texture": "url('/assets/images/landing/gastap-texture.png')",
-        "tokentap-texture":
-          "url('/assets/images/landing/tokentap-texture.png')",
-        "prizetap-texture":
-          "url('/assets/images/landing/prizetap-texture.png')",
-        "prizetap-raffle-texture":
-          "url('/assets/images/prize-tap/raffle-win.svg')",
-        "learntap-texture": "url('/assets/images/landing/learn-tap-bg.svg')",
-        "launchtap-texture":
-          "url('/assets/images/landing/launchtap-texture.png')",
-        "staketap-texture":
-          "url('/assets/images/landing/staketap-texture.png')",
-        "what-is-unitap": "url('/assets/images/landing/what-is-unitap.png')",
-        "nft-texture": "url('/assets/images/landing/genesis-nft.png')",
-        "donate-texture": "url('/assets/images/landing/donate-texture.png')",
-        "donate-texture-s": "url('/assets/images/donate/donate-texture.svg')",
-        "donate-texture-p": "url('/assets/images/donate/donate-texture.png')",
-        "stats-texture": "url('/assets/images/landing/stats-texture.png')",
-        "soft-primary":
-          "linear-gradient(90deg, #1F3A3E 0%, rgba(28, 32, 44, 0.50) 33.85%, rgba(27, 27, 41, 0.50) 63.02%, rgba(77, 52, 81, 0.60) 100%)",
         "g-twitter":
           "linear-gradient(91.35deg, #03A9F4 -0%, #DFFFF5 42.19%, #03E6F4 100%)",
         "g-primary":
@@ -139,48 +157,101 @@ const config: Config = {
         "br-pre-enrollment":
           "linear-gradient(274.27deg, #383857 9.67%, rgba(167, 159, 229, 0.5) 96.26%);",
       },
-
       dropShadow: {
         "primary-xl": "0px 8px 18px rgba(81, 88, 246, 0.15)",
       },
       zIndex: {
-        100: "100",
+        "100": "100",
       },
       keyframes: {
         wiggle: {
-          "0%": { transform: "rotate(-8deg)" },
-          "50%": { transform: "rotate(8deg)" },
-          "100%": { transform: "rotate(0deg)" },
+          "0%": {
+            transform: "rotate(-8deg)",
+          },
+          "50%": {
+            transform: "rotate(8deg)",
+          },
+          "100%": {
+            transform: "rotate(0deg)",
+          },
         },
         flip: {
-          "0%, 50%": { transform: "scaleX(-1)" },
-          "25%, 75%": { transform: "scaleX(1)" },
+          "0%, 50%": {
+            transform: "scaleX(-1)",
+          },
+          "25%, 75%": {
+            transform: "scaleX(1)",
+          },
         },
         rocket: {
-          "0%": { transform: "translateY(0)" },
-          "100%": { transform: "translateY(-15px)" },
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "100%": {
+            transform: "translateY(-15px)",
+          },
         },
         fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
         },
         fadeToggle: {
-          "0%": { opacity: "0" },
-          "10%": { opacity: "1" },
-          "95%": { opacity: "1" },
-          "100%": { opacity: "0" },
+          "0%": {
+            opacity: "0",
+          },
+          "10%": {
+            opacity: "1",
+          },
+          "95%": {
+            opacity: "1",
+          },
+          "100%": {
+            opacity: "0",
+          },
         },
         blinking: {
-          "50%": { opacity: ".3" },
+          "50%": {
+            opacity: ".3",
+          },
         },
         fadeInOut: {
-          "0%": { opacity: "1" },
-          "50%": { opacity: "0" },
-          "100%": { opacity: "1" },
+          "0%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
         },
         changeColor: {
-          "0%": { backgroundColor: "red" },
-          "50%": { backgroundColor: "yellow" },
+          "0%": {
+            backgroundColor: "red",
+          },
+          "50%": {
+            backgroundColor: "yellow",
+          },
+        },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
       animation: {
@@ -192,16 +263,24 @@ const config: Config = {
         fadeInOut: "fadeInOut 2s ease-in-out infinite",
         fadeToggle: "fadeToggle 5s ease-in-out infinite",
         changeColor: "changeColor 1s ease-in",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       transitionProperty: {
         height: "height",
         spacing: "margin, padding",
       },
       transitionDuration: {
-        0: "0ms",
-        250: "250ms",
+        "0": "0ms",
+        "250": "250ms",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
